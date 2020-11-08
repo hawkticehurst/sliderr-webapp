30e398137403f7136605a47aa625e83f790827ce
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-11-08 01:46:12 -0800

    Update SliderControls component

--- src/components/controller/controls/SliderControls.tsx
+++ src/components/controller/controls/SliderControls.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import NavBar from "../../common/NavBar";
 import WideButton from "../../common/WideButton";
+import SliderController from "./SliderController";
+import { HiArrowLeft, HiArrowRight } from "react-icons/hi";
+import { BiHomeAlt } from "react-icons/bi";
 import "./SliderControls.css";
 
 function SliderControls() {
@@ -13,6 +16,19 @@ function SliderControls() {
           alert("create new move");
         }}
       />
+      <SliderController title="Acceleration" defaultVal={75} />
+      <SliderController title="Speed" defaultVal={35} />
+      <section className="left-right-button-container">
+        <button className="circle-button">
+          <HiArrowLeft className="circle-button-icon" />
+        </button>
+        <button className="circle-button">
+          <BiHomeAlt className="circle-button-icon" />
+        </button>
+        <button className="circle-button">
+          <HiArrowRight className="circle-button-icon" />
+        </button>
+      </section>
     </section>
   );
 }
