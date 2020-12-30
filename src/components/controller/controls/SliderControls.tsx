58cdea58ab94b2c8429f111ecfe5a99d1ef7fb8b
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-12-29 21:46:58 -0800

    Import bluetooth util functions into SliderControls component

--- src/components/controller/controls/SliderControls.tsx
+++ src/components/controller/controls/SliderControls.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import NavBar from "../../common/NavBar";
-import WideButton from "../../common/WideButton";
 import SliderController from "./SliderController";
+import {
+  stopSlider,
+  moveSliderLeft,
+  moveSliderRight,
+  moveSliderHome,
+  setSliderAcceleration,
+  setSliderSpeed,
+} from "../../../utils/bluetooth";
 import { HiArrowLeft, HiArrowRight } from "react-icons/hi";
 import { BiHomeAlt } from "react-icons/bi";
 import "./SliderControls.css";
@@ -10,22 +17,30 @@ function SliderControls() {
   return (
     <section id="controls-container">
       <NavBar />
-      <WideButton
-        buttonText="Create New Move"
-        buttonCallback={() => {
-          alert("create new move");
-        }}
+      <SliderController
+        title="Acceleration"
+        onChangeFunc={setSliderAcceleration}
+        defaultVal={75}
       />
-      <SliderController title="Acceleration" defaultVal={75} />
-      <SliderController title="Speed" defaultVal={35} />
+      <SliderController
+        title="Speed"
+        onChangeFunc={setSliderSpeed}
+        defaultVal={50}
+      />
       <section className="left-right-button-container">
-        <button className="circle-button">
+        <button
+          className="circle-button"
+          onMouseDown={moveSliderLeft}
+          onMouseUp={stopSlider}>
           <HiArrowLeft className="circle-button-icon" />
         </button>
-        <button className="circle-button">
+        <button className="circle-button" onClick={moveSliderHome}>
           <BiHomeAlt className="circle-button-icon" />
         </button>
-        <button className="circle-button">
+        <button
+          className="circle-button"
+          onMouseDown={moveSliderRight}
+          onMouseUp={stopSlider}>
           <HiArrowRight className="circle-button-icon" />
         </button>
       </section>
